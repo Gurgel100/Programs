8ff70e1645eb93269c329bcae6555ecfc5e79b20
Gurgel100 <baesae@hotmail.ch>
2017-05-27 21:41:45 +0200

    Added first external program: echo

---
+++ echo/main.c
@@ -0,0 +1,23 @@
+/*
+ * main.c
+ *
+ *  Created on: 28.10.2014
+ *      Author: pascal
+ */
+
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	for(int i = 1; i < argc; i++)
+	{
+		if(i == 1)
+			printf("%s", argv[i]);
+		else
+			printf(" %s", argv[i]);
+	}
+	putchar('\n');
+	return EXIT_SUCCES;
+}
